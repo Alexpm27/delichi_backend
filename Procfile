e5b45c8495446ef5fa6d3ed0b798114a87f38878
Alexpm27 <213523@ids.upchiapas.edu.mx>
2024-04-08 21:02:40 -0600

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar ./target/delichi-api.jar
---
+++ src/main/java/com/ampei/delichi/DelichiApplication.java
@@ -0,0 +1,13 @@
+package com.ampei.delichi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelichiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelichiApplication.class, args);
+    }
+
+}
